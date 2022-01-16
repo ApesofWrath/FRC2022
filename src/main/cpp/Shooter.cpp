0f33c6ab588f4b4a337a22024db32e29e21543ce
superspeeder <andy@delusion.org>
2022-01-15 17:10:41 -0800

    fix seg fault

--- src/main/cpp/Shooter.cpp
+++ src/main/cpp/Shooter.cpp
@@ -1,7 +1,8 @@
 #include <Shooter.hpp>
+#include <frc/smartdashboard/SmartDashboard.h>
 
 Shooter::Shooter() {
-    
+    m_Motor = std::make_shared<TalonFX>(0);
 }
 
 void Shooter::shoot() {
@@ -28,28 +29,33 @@ void Shooter::reverse() {
 void Shooter::shooterStateMachine() {
     switch(m_State) {
         case ShooterState::Init:
+            frc::SmartDashboard::PutString("ShooterState", "Init");
             m_LastState = ShooterState::Init;
             m_State = ShooterState::Stop;
         break;
         case ShooterState::Stop:
+            frc::SmartDashboard::PutString("ShooterState", "Stop");
             if (m_LastState != ShooterState::Stop) {
                 stop();
             }
             m_LastState = ShooterState::Stop;
         break;
         case ShooterState::Shoot:
-            if (m_LastState != ShooterState::Stop) {
+            frc::SmartDashboard::PutString("ShooterState", "Shoot");
+            if (m_LastState != ShooterState::Shoot) {
                 shoot();
             }
-            m_LastState = ShooterState::Stop;
+            m_LastState = ShooterState::Shoot;
         break;
         case ShooterState::Waiting:
+            frc::SmartDashboard::PutString("ShooterState", "Waiting");
             if (m_LastState != ShooterState::Waiting) {
                 waiting();
             }
             m_LastState = ShooterState::Waiting;
         break;
         case ShooterState::Reverse:
+            frc::SmartDashboard::PutString("ShooterState", "Reverse");
             if (m_LastState != ShooterState::Reverse) {
                 reverse();
             }
