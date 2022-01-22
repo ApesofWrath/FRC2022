13f20da4cca2cae0be6997569beb2c27ef09e892
Brendan Donohue <brendandonohue2022@gmail.com>
2022-01-21 19:33:53 -0800

    Add hood state machine and related methods

--- src/main/cpp/Hood.cpp
+++ src/main/cpp/Hood.cpp
@@ -4,5 +4,47 @@
 // use buttons 4 and 5 for hood adjustment
 
 Hood::Hood() {
-    h_Motor = std::make_shared<TalonFX>(0);
+    m_Motor = std::make_shared<TalonFX>(0);
+}
+
+void Hood::HoodStateMachine() {
+    switch (m_State) {
+        case HoodState::Init:
+            frc::SmartDashboard::PutString("HoodState", "Init");
+            m_LastState = HoodState::Init;
+            m_State = HoodState::Init;
+        break;
+        case HoodState::LowerWall:
+            frc::SmartDashboard::PutString("HoodState", "LowerWall");
+            if (m_LastState != HoodState::LowerWall) {
+
+            }
+            m_LastState = HoodState::LowerWall;
+        break;
+        case HoodState::UpperFarWall:
+            frc::SmartDashboard::PutString("HoodState", "UpperFarWall");
+            if (m_LastState != HoodState::UpperFarWall) {
+                
+            }
+            m_LastState = HoodState::UpperFarWall;
+        break;
+        case HoodState::UpperLaunchpad:
+            frc::SmartDashboard::PutString("HoodState", "UpperLaunchpad");
+            if (m_LastState != HoodState::UpperLaunchpad) {
+                
+            }
+            m_LastState = HoodState::UpperLaunchpad;
+        break;
+        case HoodState::UpperWall:
+            frc::SmartDashboard::PutString("HoodState", "UpperWall");
+            if (m_LastState != HoodState::UpperWall) {
+              
+            }
+            m_LastState = HoodState::UpperWall;
+        break;
+    }
+}
+
+HoodState Hood::getState() {
+    return m_State;
 }
--- src/main/include/Hood.hpp
+++ src/main/include/Hood.hpp
@@ -2,9 +2,18 @@
 
 #include <ctre/Phoenix.h>
 #include <memory>
+#include <unordered_map>
 
 constexpr float hoodSpeed = .1;
 constexpr int hoodMotor = 1;
+
+enum class HoodState {
+    Init,
+    UpperWall,
+    UpperFarWall,
+    UpperLaunchpad,
+    LowerWall
+};
 
 
 class Hood {
@@ -12,8 +21,27 @@ public:
 
     Hood();
 
-private:
 
-    std::shared_ptr<TalonFX> h_Motor;
+    inline float getTargetPosition() { return m_TargetPosition; };
+    void setTargetPosition(float angle);
+
+    void HoodStateMachine();
+
+    void setState(HoodState state) {m_State = state;}
+    HoodState getState();
+
+private:
+    std::unordered_map<HoodState,float> m_PositionMap;
+    std::shared_ptr<TalonFX> m_Motor;
+
+    float m_TargetPosition;
+
+    HoodState m_LastState, m_State = HoodState::Init;
+
+    const float kP = 0;
+    const float kI = 0;
+    const float kD = 0;
+    const float kF = 0;
+
 };
 
